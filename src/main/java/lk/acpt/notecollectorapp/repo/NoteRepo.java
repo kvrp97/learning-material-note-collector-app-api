4da203fd8ed1e7f0031da2b7f6b4baf51764c6aa
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-04-02 10:41:23 +0530

    @EnableJpaRepositories @Repository added

--- src/main/java/lk/acpt/notecollectorapp/repo/NoteRepo.java
+++ src/main/java/lk/acpt/notecollectorapp/repo/NoteRepo.java
@@ -2,6 +2,10 @@ package lk.acpt.notecollectorapp.repo;
 
 import lk.acpt.notecollectorapp.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
+@Repository
 public interface NoteRepo extends JpaRepository<Note,Integer> {
 }
